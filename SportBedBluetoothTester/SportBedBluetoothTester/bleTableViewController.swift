bb19a73a37a6e83736ccd658269e5161f8fa3893
Raymond Link <chairiot.control@gmail.com>
2021-10-26 22:51:01 -0500

    removal of error code

--- SportBedBluetoothTester/SportBedBluetoothTester/bleTableViewController.swift
+++ SportBedBluetoothTester/SportBedBluetoothTester/bleTableViewController.swift
@@ -1,11 +1,11 @@
 //
 //  bleTableViewController.swift
-//  bleModule
+//  SportBedBluetoothTester
 //
-//  Created by Casey Brittain on 4/12/15.
-//  Copyright (c) 2015 Casey Brittain. All rights reserved.
+//  Created by Raymond Link on 10/26/21.
 //
 
+import Foundation
 import UIKit
 import CoreBluetooth
 
